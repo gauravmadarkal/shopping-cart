499c038f25b80eda2dc855d8f747c5185306afaa
gaurav.m <gaurav.madarkal@betsol.com>
2020-07-19 16:39:26 +0530

    added order details page

--- src/component/orders.js
+++ src/component/orders.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Button } from 'antd';
 import { connect } from 'react-redux';
-const columns = [
+import _ from 'lodash'
+const OrdersColumns = [
     {
         title: 'Order Name',
         dataIndex: 'name',
@@ -10,30 +11,82 @@ const columns = [
     },
     {
         title: 'Order Id',
-        dataIndex: 'id',
-        key: 'id'
+        dataIndex: 'orderId',
+        key: 'orderId'
     },
     {
         title: 'Order Date',
-        dataIndex: 'date',
-        key: 'date'
+        dataIndex: 'orderDate',
+        key: 'orderDate'
+    },
+    {
+        title: 'Status',
+        dataIndex: 'status',
+        key: 'status',
+        render: text => <Button>{text}</Button>
     }
 ]
+const ProductColumns = [
+    {
+        title: 'Product Id',
+        dataIndex: 'productId',
+        key: 'productId'
+    },
+    {
+        title: 'Quantity',
+        dataIndex: 'quantity',
+        key: 'quantity'
+    },
+]
+var customer = null;
+var orders = null;
 class Orders extends React.Component{
+    constructor(props) {
+        super(props);
+        this.showOrderDetails = this.showOrderDetails.bind(this);
+        this.state = {
+            selectedOrder: null
+        }
+    }
     componentDidMount() {
         
     }
+    showOrderDetails(event, orderId) {
+        var products = [];
+        _.find(customer.orders, function (order) {
+            if (order.orderId == orderId)
+                products.push(order);
+        })
+        console.log(`${JSON.stringify(products)}`);
+        this.setState({
+            selectedOrder: products
+        })
+    }
     render() {
-        console.log("hi ",this.props.match.params.id);
+        console.log("hi ", this.props.match.params.id);
+        var id = this.props.match.params.id;
+        // var obj = _.find(this.props.customers, { 'customerId': this.props.match.params.id })
+        customer = _.find(this.props.customers, function (customer) {
+            if (customer.customerId == id)
+                return customer;
+        });
+        if (customer != null) {
+            orders = _.uniqBy(customer.orders, 'orderId');
+            orders = _.orderBy(orders, ['orderId'], ['asc']);
+        }
         return (
             <div className="container">
                 <h4 style={{textAlign:"left", marginBottom:"20px"}}>List of orders for customer id: {this.props.match.params.id}</h4>
                 <div className="row">
-                    <div className="col-md-6">
-                        <Table columns={columns}/>
+                    <div className="col-md-8">
+                        {customer ? <Table columns={OrdersColumns} dataSource={orders} onRow={(record, index) => {
+                            return {
+                                onClick: (event) => {this.showOrderDetails(event, record.orderId)}
+                            }
+                        }}/> : <div></div>}
                     </div>
-                    <div className="col-md-6">
-                        <h1>hii</h1>
+                    <div className="col-md-4">
+                        <Table columns={ProductColumns} dataSource={this.state.selectedOrder}/>
                     </div>
                 </div>
             </div>
